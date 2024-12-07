9d1e834aad3ae608fb23860d075a84e6755983cc
jang yeoju <duwn0105@naver.com>
2024-12-08 01:31:30 +0900

    font fix2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,22 @@
 "use client"
 import "./globals.css";
+import localFont from 'next/font/local'
 
-export default function RootLayout({ children }) {
+const pretendard = localFont({
+  src: '../app/style/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+})
+
+
+export default function RootLayout({children}) {
   return (
-    <html lang="en">
-    <body>
-      {children}
-    </body>
-  </html>
+    <html lang="en" className={`${pretendard.variable} font-pretendard`}>
+      <body>
+        {children}
+      </body>
+    </html>
   );
 }
 
