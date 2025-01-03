cfcdfc7d948a17c6e55539c7f05ccb04017f248c
jang yeoju <duwn0105@naver.com>
2025-01-04 02:05:46 +0900

    merge admin

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@
 import './globals.css';
 
 import localFont from 'next/font/local';
+import Script from 'next/script';
 
 const pretendard = localFont({
   src: '../style/fonts/PretendardVariable.woff2',
@@ -20,11 +21,20 @@ const montserrat = localFont({
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${pretendard.className} ${montserrat.className}`}
-    >
-      <body>{children}</body>
-    </html>
+    <>
+      <html
+        lang="en"
+        className={`${pretendard.className} ${montserrat.className}`}
+      >
+        <head>
+          <meta name="naver-site-verification" content="bc5dde5edbbb6eb963169a2a2b3cd633f3e383c6" />
+        </head>
+        <body>
+          <Script>
+          </Script>
+          {children}
+        </body>
+      </html>
+    </>
   );
 }
